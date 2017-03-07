84aa266c637b2098a8a5f1c78c99ee49d4819b41
ZeroInfinity <infinitezero345@gmail.com>
2017-03-07 23:26:36 +0000

    Initial Import
    Added source code + compiled executable for Windows OS.

---
+++ schedutilCalculator.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+ *
+ * Copyright (C) ZeroInfinity@XDA 2017
+ * Author: ZeroInfinity <infinitezero345@gmail.com>
+ * 
+ */
+int main(){
+	int bitshift = 0, freqTotal = 0, freq = 1, shift = 0;
+	float percentage = 0, tippingPoint = 0;
+	printf("enter max freq: ");
+	scanf("%d",&freq);
+	while (freq != 0) {
+		printf("enter bit shift value: ");
+		scanf("%d",&shift);
+		bitshift = freq >> shift;
+		freqTotal = freq + bitshift;
+		percentage = (float)freqTotal / (float)freq;
+		tippingPoint = (float)freq / (float)freqTotal;
+		printf("bitshift Total: %d\nfreqTotal: %d\nC: %f\nTipping Point: %f\n\n", bitshift, freqTotal, percentage, tippingPoint);
+		printf("enter max freq: ");
+		scanf("%d",&freq);
+	}
+}
+
