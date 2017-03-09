52ad5e114fbfc4c0180a0a72ef3fd2e641df51da
ZeroInfinity <infinitezero345@gmail.com>
2017-03-09 00:08:56 +0000

    Add feature to calculate CPU load and next frequency seleciton

--- schedutilCalculator.cpp
+++ schedutilCalculator.cpp
@@ -8,18 +8,26 @@
  * 
  */
 int main(){
-	int bitshift = 0, freqTotal = 0, freq = 0, shift = 0;
-	float percentage = 0, tippingPoint = 0;
+	int bitshift = 0, freqTotal = 0, freq = 1, shift = 0, util = 0, max = 0;
+	float percentage = 0, tippingPoint = 0, load = 0, nextFreq = 0, loadperc = 0;
 	printf("enter max freq: ");
 	scanf("%d",&freq);
 	while (freq != 0) {
 		printf("enter bit shift value: ");
 		scanf("%d",&shift);
+		printf("enter max capacity of CPU: ");
+		scanf("%d",&max);
+		printf("enter CPU utilisation: ");
+		scanf("%d",&util);
 		bitshift = freq >> shift;
 		freqTotal = freq + bitshift;
 		percentage = (float)freqTotal / (float)freq;
 		tippingPoint = (float)freq / (float)freqTotal;
-		printf("bitshift Total: %d\nfreqTotal: %d\nC: %f\nTipping Point: %f\n\n", bitshift, freqTotal, percentage, tippingPoint);
+		load = (float)util / (float)max;
+		loadperc = load * 100;
+		nextFreq = freqTotal * load;
+		printf("bitshift Total: %d\nfreqTotal: %d\nC: %f\nTipping Point: %f\n", bitshift, freqTotal, percentage, tippingPoint);
+		printf("CPU Load: %f\nnextFreq: %f\n\n", loadperc, nextFreq);
 		printf("enter max freq: ");
 		scanf("%d",&freq);
 	}
